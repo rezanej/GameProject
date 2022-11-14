d9dfa37a38fd48dba9f18e8bbf8c936160da19d1
rezanejL <rezanej666@gmail.com>
2022-11-14 15:48:56 +0330

    adding restrictions for kunai

--- Kunai.py
+++ Kunai.py
@@ -4,10 +4,10 @@ from Setting import *
 class Kunai(pygame.sprite.Sprite):
     def __init__(self,x,y,player):
         super().__init__()
+        self.lifeTime=KunaiLifetime
         if player.left:
             self.direction=-1
             self.image = KunaiImgaeLeft
-
         else:
             self.direction=1
             self.image=KunaiImgae
@@ -16,7 +16,14 @@ class Kunai(pygame.sprite.Sprite):
         self.speed=KunaiSpeed
 
     def update(self):
+        self.move()
+        self.life()
+    def move(self):
         if self.direction>0:
             self.rect.x+=KunaiSpeed
         elif self.direction<0:
             self.rect.x-=KunaiSpeed
+    def life(self):
+        self.lifeTime -= 1
+        if self.lifeTime < 0:
+            self.kill()
